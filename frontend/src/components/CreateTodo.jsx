2a781458a8e17cf43616815a1467c3a6d6d5c541
Prathyarthi <prathyarti@gmail.com>
2024-01-05 18:52:23 +0530

    fetched data from backend, added components

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,35 @@
-export function CreateTodo() {
-    return <div>
-        <input type="text" placeholder="title" /><br /><br />
-        <input type="text" placeholder="description" /><br /><br />
+import { useState } from "react"
 
-        <button>Add a Todo!</button>
+export function CreateTodo() {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+    return <div>
+        <input style={{ margin: 10, padding: 10 }} type="text" placeholder="title" onChange={function (e) {
+            value = e.target.value
+            setTitle(e.target.value)
+        }} /><br /><br />
+        <input style={{ margin: 10, padding: 10 }} type="text" placeholder="description" onChange={function (e) {
+            value = e.target.value
+            setDescription(e.target.value)
+        }} /><br /><br />
+
+        <button onClick={
+            fetch('http://localhost:3000/todo', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: title,
+                    description: description,
+                }),
+                headers: {
+                    "content-type": "application/json"
+                }
+            })
+                .then(async function (res) {
+                    const json = await res.json()
+                    console.log(json.todos)
+                    alert("Todo Added!")
+                })
+        } style={{ margin: 10, padding: 10 }} >Add a Todo!</button>
     </div>
 }
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,5 +1,11 @@
-export function Todos() {
+export function Todos({ todos }) {
     return <div>
-
+        {todos.map(function (todo) {
+            return <div>
+                <h2>{todos.title}</h2>,
+                <h2>{todos.description}</h2>,
+                <button>{todo.completed == true ? "completed" : "Mark as done!"}</button>
+            </div>
+        })}
     </div>
 }
